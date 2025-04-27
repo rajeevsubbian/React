a0a2172ec0b22471a3c45b8141eccff38a32db19
Rajeev Subbian <rajeev.subbian@gmail.com>
2025-04-27 15:49:36 +0100

    finish of user-profile exercise

--- user-profile/src/App.jsx
+++ user-profile/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const userprofile = [
+    {
+      id: 1,
+      name: "Rajeev Subbian",
+      email: "rajeev.subbian@email.com",
+      bio: "Software Engineer with a passion for web development.",
+    },
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="user-profile">
+        {userprofile.map((user, index) => (
+          <User
+            key={user.id}
+            name={user.name}
+            email={user.email}
+            bio={user.bio}
+          />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+function User({ name, email, bio }) {
+  // console.log(props);
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>Email: {email}</p>
+      <p>Bio: {bio}</p>
+    </div>
+  );
+}
