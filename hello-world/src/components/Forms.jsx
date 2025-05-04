beade4280917cea79f0d10cdc4fd56919ff62b80
Rajeev Subbian <rajeev.subbian@gmail.com>
2025-05-04 23:52:40 +0100

    Save names in list in form

--- hello-world/src/components/Forms.jsx
+++ hello-world/src/components/Forms.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
+
 function NameForm() {
+  const [name, setName] = useState();
+  const [names, setNames] = useState([]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setNames([...names, name]);
+    setName('');
+  }
+
   return (
     <div className="container mt-4">
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <input
           className="form-control"
           type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
           placeholder="Enter your name"
         />
         <button className="btn btn-primary mt-2">Submit</button>
       </form>
+      <ul>
+        {names.map((nameItem, index) => (
+            <li key={index} className="list-group-item">
+                {nameItem}
+            </li>
+        ))}
+      </ul>
     </div>
   );
 }
