7b34dd832e06db3bc146c391920be7c139ab3498
Rajeev Subbian <rajeev.subbian@gmail.com>
2025-05-05 20:29:56 +0100

    use effect to manage lifecycle of component (control time interval)

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -3,44 +3,34 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  // getData();
-
-  useEffect(() => {
-    async function getData() {
-      setIsLoading(true);
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      const jsonData = await response.json();
-
-      setData(jsonData);
-      setIsLoading(false);
-    }
-
-    getData();
-  }, []);
-
   return (
     <div className="container pt-2">
-      <DataFetching data={data} isLoading={isLoading} />
+      <AdjustableTimer />
     </div>
   );
 }
-
 export default App;
 
-function DataFetching({ data, isLoading }) {
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+function AdjustableTimer() {
+  const [seconds, setSeconds] = useState(0);
+  const [intervalMs, setIntervalMs] = useState(1000);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, intervalMs);
+    return () => clearInterval(interval);
+  }, [intervalMs]);
+
   return (
     <div>
-      {data.map((post) => (
-        <div key={post.id}>{post.title}</div>
-      ))}
+      <div>Seconds: {seconds}</div>
+      <input
+        type="number"
+        value={intervalMs}
+        onChange={(e) => setIntervalMs(Number(e.target.value))}
+      />
+      <label>Adjust interval (ms)</label>
     </div>
   );
 }
