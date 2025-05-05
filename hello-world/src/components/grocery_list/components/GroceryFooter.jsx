4428c9f7e4edae50795e4aebcecc7d1279242853
Rajeev Subbian <rajeev.subbian@gmail.com>
2025-05-05 14:00:43 +0100

    Form updated to add the items

---
+++ hello-world/src/components/grocery_list/components/GroceryFooter.jsx
@@ -0,0 +1,11 @@
+function GroceryFooter() {
+  return (
+    <div className="footer">
+      <footer className="bg-light text-dark text-center p3">
+        <p className="display-7"> Footer</p>
+      </footer>
+    </div>
+  );
+}
+
+export default GroceryFooter;
